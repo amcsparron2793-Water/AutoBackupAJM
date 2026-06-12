017fa563309f6818dff3647a7e3a09e91e734810
Andrew McSparron <Amcsparron@albanyny.gov>
2026-06-12 14:04:33 -0400

    Annotate TODO with multithreading suggestion for folder hashing/checking in `AutoBackup` class

--- AutoBackupAJM/auto_backup_ajm.py
+++ AutoBackupAJM/auto_backup_ajm.py
@@ -19,7 +19,7 @@ from hashlib import md5
 import questionary
 
 
-# TODO: hash and check folders for changes before backup
+# TODO: hash and check folders for changes before backup - multithread this?
 class AutoBackup:
     """
     Class to handle automated backup of a file.
