cef3659e3b067c3a4ca01874975adc5fce848fcb
Andrew McSparron <Amcsparron@albanyny.gov>
2026-06-15 11:34:48 -0400

    Introduce `HasherFactory` for dynamic hasher instantiation and testing
    - Added a `HasherFactory` class to dynamically instantiate `FileHasher` or `DirectoryHasher` based on input type.
    - Enhanced testing flow for both factory and non-factory hashing.
    - Refactored `main` logic for better modularity and testability.

--- AutoBackupAJM/hasher.py
+++ AutoBackupAJM/hasher.py
@@ -1,6 +1,6 @@
 from hashlib import md5
 from pathlib import Path
-from typing import Union, Tuple, Generator
+from typing import Union, Tuple, Generator, Optional
 
 
 class FileHasher:
@@ -74,12 +74,66 @@ class DirectoryHasher(FileHasher):
                 print(f"Skipping subdir {file.name}...")
 
 
-if __name__ == "__main__":
-    file_hasher = FileHasher('./_version.py')
-    dir_hasher = DirectoryHasher('../../AutoBackupAJM')
+class HasherFactory:
+    FILE_HASHER_CLASS = FileHasher
+    DIRECTORY_HASHER_CLASS = DirectoryHasher
 
-    print(file_hasher.hash_file())
+    @classmethod
+    def inst_hasher_class(cls, input_path: Union[str, Path], **kwargs):
+        if Path(input_path).is_file():
+            return cls.FILE_HASHER_CLASS(input_path, **kwargs)
+        elif Path(input_path).is_dir():
+            return cls.DIRECTORY_HASHER_CLASS(input_path, **kwargs)
+        else:
+            raise ValueError("input_path must be a file or directory")
+
+    @classmethod
+    def validate_input_path(cls, input_path: Optional[Union[str, Path]], *args, **kwargs):
+        if input_path:
+            return cls.inst_hasher_class(input_path, *args, **kwargs)
+        else:
+            raise ValueError("Must specify input_path")
+
+    def __new__(cls, *args, **kwargs):
+        input_path: Optional[Union[str, Path]] = kwargs.pop("input_path", None)
+        return cls.validate_input_path(input_path, *args, **kwargs)
+
+
+def _test_hashing(hasher):
+    print(f"testing with {hasher.__class__.__name__}\n")
+    if hasher.__class__ in [FileHasher, HasherFactory]:
+        print(hasher.hash_file())
+
+    if hasattr(hasher, 'hash_directory'):
+        for x in hasher.hash_directory():
+            print(x)
     print('\n')
 
-    for x in dir_hasher.hash_directory():
-        print(x)
+
+def _test_non_factory_hashing():
+    file_hasher = FileHasher(test_file)
+    dir_hasher = DirectoryHasher(test_dir)
+    _test_hashing(file_hasher)
+    _test_hashing(dir_hasher)
+
+
+def _test_factory_hashing(input_path: Optional[Union[str, Path]]):
+    factory_hasher = HasherFactory(input_path=input_path)
+    print(factory_hasher.__class__.__name__)
+    _test_hashing(factory_hasher)
+
+
+if __name__ == "__main__":
+    test_non_factory = False
+    test_file = './_version.py'
+    test_dir = '../../AutoBackupAJM'
+
+    if test_non_factory:
+        _test_non_factory_hashing()
+    else:
+        _test_factory_hashing(test_file)
+        _test_factory_hashing(test_dir)
+
+
+
+
