07691a27d992d72d6d7780853f81c0d69283c3d5
Andrew McSparron <Amcsparron@albanyny.gov>
2026-06-23 08:43:07 -0400

    Refactor `continuous_monitor`: extract `_monitor_loop`, simplify `not_due_notified` handling, and enhance parameter typing in constructor.

--- AutoBackupAJM/auto_backup_continuous_check.py
+++ AutoBackupAJM/auto_backup_continuous_check.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from time import sleep
+from typing import Union
 
 from AutoBackupAJM import AutoBackup
 
@@ -7,28 +8,33 @@ from AutoBackupAJM import AutoBackup
 # TODO: use the_sandman for sleeping?
 # TODO: refresh DATE_TODAY every hour for continuous?
 class AutoBackupContinuousCheck(AutoBackup):
+    def __init__(self, source_path: Union[Path, str], backup_dir_path_root: Union[Path, str], **kwargs):
+        super().__init__(source_path, backup_dir_path_root, **kwargs)
+        self.not_due_notified = False
+
     def _log_intro_and_warnings(self):
         self._logger.info("Continuous Backup Monitor started...")
         self._logger.info("press ctrl+c to exit")
         self._logger.warning("_overwrite_protection_check() disabled for continuous monitor")
         self._logger.warning("_make_backup_dir_path_root_question() always returns True for continuous monitor")
 
+    def _monitor_loop(self):
+        if self.due_for_backup:
+            self._logger.info("Attempting backup...")
+            self.backup()
+            self.not_due_notified = False
+        else:
+            if not self.not_due_notified:
+                self._logger.info("no backup necessary")
+                self.not_due_notified = True
+        self.sleep(10)  # TODO: make this based on self.backup_frequency
+
     def continuous_monitor(self):
         self._log_intro_and_warnings()
 
-        not_due_notified = False
         while True:
             try:
-                if self.due_for_backup:
-                    self._logger.info("attempting backup...")
-                    self.backup()
-                    not_due_notified = False
-                else:
-                    if not not_due_notified:
-                        self._logger.info("no backup necessary")
-                        not_due_notified = True
-                self.sleep(10)  # TODO: make this based on self.backup_frequency
-
+                self._monitor_loop()
             except KeyboardInterrupt as e:
                 self._logger.warning("user interrupted backup process, exiting...")
                 break
