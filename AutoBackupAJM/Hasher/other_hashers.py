8f9f909c1aa138c8202bd81d9c496f0b9a4040b4
Andrew McSparron <Amcsparron@albanyny.gov>
2026-06-25 15:24:50 -0400

    Refactor `ArchiveFileHasher` and `ArchiveDirectoryHasher`: enhance content hashing with `_hash_contents`, improve archive extraction, and refine exception handling for `input_path`.

--- AutoBackupAJM/Hasher/other_hashers.py
+++ AutoBackupAJM/Hasher/other_hashers.py
@@ -14,34 +14,50 @@ class ArchiveFileHasher(LargeFileHasher):
         super().__init__(input_path, **kwargs)
         kwargs.setdefault('logger', self._logger)
         self.unzip_and_hash_contents = kwargs.get("unzip_and_hash_contents", False)
+        self.extractor = ArchiveExtractor(self.input_path, **kwargs)
 
     @LargeFileHasher.input_path.setter
     def input_path(self, value: Union[str, Path]):
         LargeFileHasher.input_path.fset(self, value)#.input_path
 
         if self._input_path.suffix not in self.__class__.ARCHIVE_FILE_TYPES:
-            raise ValueError(f"input_path must be an archive file, not {self._input_path.suffix}")
-            raise ValueError(f"input_path must be an archive file, not {self._input_path.suffix or 'a directory'}")
+            raise ValueError(f"input_path must be an archive file, "
+                             f"not {self._input_path.suffix or 'a directory'}")
+
+    def _hash_contents(self, archive_contents, **kwargs):
+        if isinstance(archive_contents, list):
+            if len(archive_contents) == 1 and archive_contents[0].is_file():
+                return self.hash_file(archive_contents[0], **kwargs)
+            else:
+                raise AttributeError("use ArchiveDirectoryHasher to hash the contents")
+        elif isinstance(archive_contents, Path):
+            return self.hash_file(archive_contents, **kwargs)
+        else:
+            raise TypeError("archive_contents must be a list of Path objects or a single Path object")
 
     def hash_archive(self, **kwargs):
         unzip_and_hash = kwargs.get("unzip_and_hash_contents", self.unzip_and_hash_contents)
         if unzip_and_hash:
-            raise NotImplementedError("unzip_and_hash_contents is not yet implemented")
+            self.extractor.extract_archive()
+            return self._hash_contents(self.extractor.extract_dir, **kwargs)
         else:
+            # hash as one file
             return self.hash_file(self.input_path, **kwargs)
         # raise NotImplementedError("hash_archive is not yet implemented")
 
 
 class ArchiveDirectoryHasher(ArchiveFileHasher, LargeDirectoryHasher):
-    def hash_archive(self, **kwargs):
-        unzip_and_hash = kwargs.get("unzip_and_hash_contents", self.unzip_and_hash_contents)
-        if unzip_and_hash:
-            self.hash_directory(**kwargs)
-        else:
-            raise NotImplementedError("unzip_and_hash_contents is not yet implemented")
+    def _hash_contents(self, archive_contents, **kwargs):
+        if isinstance(archive_contents, list):
+            if len(archive_contents) == 1 and archive_contents[0].is_file():
+                raise AttributeError("use ArchiveFileHasher to hash the contents")
+        elif isinstance(archive_contents, Path):
+            # FIXME: this causes ValueError: input_path must be an archive file, not a directory
+            self.input_path = archive_contents
+        return self.hash_directory(**kwargs)
 
 
 if __name__ == "__main__":
-    AH = ArchiveFileHasher('../../Misc_Project_Files/HostedFeatureStorage.zip')
-    archive_hash = AH.hash_archive(unzip_and_hash_contents=False)
-    print(archive_hash)
+    AH = ArchiveDirectoryHasher('../../Misc_Project_Files/HostedFeatureStorage.zip')
+    archive_hash = AH.hash_archive(unzip_and_hash_contents=True)
+    print([x for x in archive_hash])
