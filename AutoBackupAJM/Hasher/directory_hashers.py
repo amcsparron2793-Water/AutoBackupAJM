2a22fe0809c3b024c63c66a88e33eb289d8efeba
Andrew McSparron <Amcsparron@albanyny.gov>
2026-06-29 14:51:18 -0400

    Refactor `DirectoryHasher`: add `_walk_directory` helper, `hash_and_record_directory`, and JSON recording of directory hashes; update `.gitignore` for JSON exclusions.

--- AutoBackupAJM/Hasher/directory_hashers.py
+++ AutoBackupAJM/Hasher/directory_hashers.py
@@ -23,23 +23,42 @@ class DirectoryHasher(FileHasher):
             raise ValueError(f"self.input_path must be a directory, to hash a file use hash_file")
         return dir_path
 
-    def hash_directory(self, **kwargs) -> Generator[Tuple[Union[Path, str], str], None, None]:
-        dir_path = self._validate_input_path_is_dir()
-        self._logger.info(f"Hashing directory {dir_path.resolve()}.")
+    def _walk_directory(self, dir_path: Path, **kwargs):
         ignore_system_dirs = kwargs.get("ignore_system_dirs", self.ignore_system_dirs)
-
-        # TODO: walk needs to be relative to the root dir
-        #  since the root dir is always going to be
-        #  different since the source is from a backup.
-
-        # TODO: multithreading?
         for current_dir, subdirs, files in dir_path.walk(): #dir_path.iterdir():
             if ignore_system_dirs:
                 if self._is_system_dir(current_dir):
                     continue
             for file in files:
                 full_path = current_dir / file
-                yield self.hash_file(full_path, **kwargs)
+                yield full_path
+
+    def hash_directory(self, **kwargs) -> Generator[Tuple[Union[Path, str], str], None, None]:
+        dir_path = self._validate_input_path_is_dir()
+        self._logger.info(f"Hashing directory {dir_path.resolve()}.")
+        kwargs.setdefault("ignore_system_dirs", self.ignore_system_dirs)
+
+        # TODO: walk needs to be relative to the root dir
+        #  since the root dir is always going to be
+        #  different since the source is from a backup.
+
+        # TODO: multithreading?
+        for fp in self._walk_directory(dir_path, **kwargs):
+            yield self.hash_file(fp, **kwargs)
+
+    def hash_and_record_directory(self, **kwargs):
+        directory_records = {}
+        for f_path, f_hash in self.hash_directory():
+            directory_records[f_hash] = f_path.as_posix()
+        self._record_directory(directory_records, **kwargs)
+
+    def _record_directory(self, directory_records: dict, **kwargs):
+        default_file_name = "directory_hashes.json"
+        default_record_path = Path(PROJECT_ROOT / "Misc_Project_Files"/ default_file_name)
+        record_path = kwargs.get("record_path", default_record_path)
+        with open(record_path, "w") as f:
+            dump(directory_records, fp=f,  indent=4)
+            self._logger.info(f"Directory hashes recorded to {record_path}")
 
 
 class LargeDirectoryHasher(LargeFileHasher, DirectoryHasher):
@@ -47,6 +66,7 @@ class LargeDirectoryHasher(LargeFileHasher, DirectoryHasher):
 
 
 if __name__ == "__main__":
-    dir_hasher = DirectoryHasher(Path("../"))
-    for x in dir_hasher.hash_directory():
-        print(x[1])
+    dir_hasher = DirectoryHasher(Path("../../Logs"))
+    dir_hasher.hash_and_record_directory()
+    # for x in dir_hasher.hash_directory():
+    #     print(x[1])
