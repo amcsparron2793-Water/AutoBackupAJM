f58a51cfb83bdf955fd94402d7a505837028d94a
Andrew McSparron <Amcsparron@albanyny.gov>
2026-06-29 14:17:09 -0400

    Add system directory handling to `DirectoryHasher` with `_is_system_dir` helper and `ignore_system_dirs` flag

--- AutoBackupAJM/Hasher/directory_hashers.py
+++ AutoBackupAJM/Hasher/directory_hashers.py
@@ -1,10 +1,21 @@
+from json import dumps, dump
 from pathlib import Path
 from typing import Generator, Tuple, Union
-
+from AutoBackupAJM import PROJECT_ROOT
 from AutoBackupAJM.Hasher.file_hashers import FileHasher, LargeFileHasher
 
 
 class DirectoryHasher(FileHasher):
+    SYSTEM_DIR_PREFIXES = ['.', '__']
+
+    def __init__(self, input_path: Path, ignore_system_dirs: bool = True, **kwargs):
+        self.ignore_system_dirs = ignore_system_dirs
+        super().__init__(input_path, **kwargs)
+
+    @classmethod
+    def _is_system_dir(cls, dir_path: Path) -> bool:
+        return dir_path.name.startswith(tuple(cls.SYSTEM_DIR_PREFIXES))
+
     def _validate_input_path_is_dir(self) -> Path:
         if self.input_path.is_dir():
             dir_path = self.input_path
@@ -15,6 +26,7 @@ class DirectoryHasher(FileHasher):
     def hash_directory(self, **kwargs) -> Generator[Tuple[Union[Path, str], str], None, None]:
         dir_path = self._validate_input_path_is_dir()
         self._logger.info(f"Hashing directory {dir_path.resolve()}.")
+        ignore_system_dirs = kwargs.get("ignore_system_dirs", self.ignore_system_dirs)
 
         # TODO: walk needs to be relative to the root dir
         #  since the root dir is always going to be
@@ -22,8 +34,9 @@ class DirectoryHasher(FileHasher):
 
         # TODO: multithreading?
         for current_dir, subdirs, files in dir_path.walk(): #dir_path.iterdir():
-            if current_dir.name.startswith("__"):
-                continue
+            if ignore_system_dirs:
+                if self._is_system_dir(current_dir):
+                    continue
             for file in files:
                 full_path = current_dir / file
                 yield self.hash_file(full_path, **kwargs)
