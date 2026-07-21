2a95c3d3ccde76d8fa72bf092b9f645bf4032fbb
Andrew McSparron <Amcsparron@albanyny.gov>
2026-07-21 15:16:02 -0400

    Add `use_big` option to `_QuickTest` for handling large directory comparisons. Update path logic in `jd` mode to utilize `use_big` paths.

--- AutoBackupAJM/utilities.py
+++ AutoBackupAJM/utilities.py
@@ -22,13 +22,17 @@ class _QuickTest:
     test_backup_json = Path(MISC_PROJECT_DIR, "HostedFeatureStorage.json")
     test_new_zip = Path(MISC_PROJECT_DIR, "HostedFeatureStorage.zip")
     test_other_zip = Path(MISC_PROJECT_DIR, "HostedFeatureStorage_Other.zip")
-    test_new_json = Path(MISC_PROJECT_DIR, "HostedFeatureStorage_Other.json")
+    test_new_json = Path(MISC_PROJECT_DIR, "ArcMap and Pro Projects.json")
     test_dir_json = Path(MISC_PROJECT_DIR, "ArcMap_and_Pro_Projects_Backup.json")
+    test_big_dir_json = Path(MISC_PROJECT_DIR, "Desktop_Backup.json")
     test_target_dir = Path("~/Desktop/ArcMap and Pro Projects").expanduser()
+    test_big_target_dir = Path("~/Desktop").expanduser()
+
     # TODO: HASHER_CLASS_MAP = dict()
     # test_target_dir = Path(MISC_PROJECT_DIR)
 
     def __init__(self, class_to_use: type, jj=False, ja=False, aa=False, jd=False, **kwargs):
+        self._use_big = kwargs.get('use_big', False)
         self._class_to_use = None
 
         self.class_to_use = class_to_use
@@ -74,8 +78,10 @@ class _QuickTest:
                                         target_archive_file=self.test_other_zip,
                                         **kwargs)
         elif self.jd:
-            self.hc = self.class_to_use(source_json=self.test_dir_json,
-                                        target_dir=self.test_target_dir,
+            src_json = self.test_dir_json if not self._use_big else self.test_big_dir_json
+            target_dir = self.test_target_dir if not self._use_big else self.test_big_target_dir
+            self.hc = self.class_to_use(source_json=src_json,
+                                        target_dir=target_dir,
                                         **kwargs)
 
     def compare_test(self):
