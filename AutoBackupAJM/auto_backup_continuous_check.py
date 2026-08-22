9a47802b32b11d51b5b39813b166885067b5628b
Andrew McSparron <Amcsparron@albanyny.gov>
2026-08-22 19:42:54 -0400

    Add `print_msg` parameter to logger calls in `BasicAutoBackupContinuousCheck` for improved messaging flexibility.

--- AutoBackupAJM/auto_backup_continuous_check.py
+++ AutoBackupAJM/auto_backup_continuous_check.py
@@ -7,25 +7,26 @@ from AutoBackupAJM import BasicAutoBackup, ExternalCompareAutoBackup, MISC_PROJE
 
 # TODO: use the_sandman for sleeping?
 # TODO: refresh DATE_TODAY every hour for continuous?
+# FIXME: needs logger work?
 class BasicAutoBackupContinuousCheck(BasicAutoBackup):
     def __init__(self, source_path: Union[Path, str], backup_dir_path_root: Union[Path, str], **kwargs):
         super().__init__(source_path, backup_dir_path_root, **kwargs)
         self.not_due_notified = False
 
     def _log_intro_and_warnings(self):
-        self._logger.info("Continuous Backup Monitor started...")
-        self._logger.info("press ctrl+c to exit")
+        self._logger.info("Continuous Backup Monitor started...", print_msg=True)
+        self._logger.info("press ctrl+c to exit", print_msg=True)
         self._logger.warning("_overwrite_protection_check() disabled for continuous monitor")
         self._logger.warning("_make_backup_dir_path_root_question() always returns True for continuous monitor")
 
     def _monitor_loop(self):
         if self.due_for_backup:
-            self._logger.info("Attempting backup...")
+            self._logger.info("Attempting backup...", print_msg=True)
             self.backup()
             self.not_due_notified = False
         else:
             if not self.not_due_notified:
-                self._logger.info("no backup necessary")
+                self._logger.info("no backup necessary", print_msg=True)
                 self.not_due_notified = True
         self.sleep(10)  # TODO: make this based on self.backup_frequency
 
