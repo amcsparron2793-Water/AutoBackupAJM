4a62b67e378f8ca3dd343a1a41bf0ba7ab12c766
Andrew McSparron <Amcsparron@albanyny.gov>
2026-09-04 14:14:09 -0400

    Refactor `custom_compare_factory` by introducing `_use_hash_file`, updating `_directory_src_targets` with improved hash file handling logic, and enhancing logging and default behaviors.

--- AutoBackupAJM/custom_compare_factory.py
+++ AutoBackupAJM/custom_compare_factory.py
@@ -92,8 +92,28 @@ class AutoBackupComparerFactory(ComparerFactory):
         return target, was_unzipped
 
     @classmethod
-    def _directory_src_targets(cls, source: Any, target: Any, **kwargs):
+    def _use_hash_file(cls, source: Any, target: Any, **kwargs):
+        logger = kwargs.setdefault('logger', getLogger(cls.__name__))
         ignore_hash_file = kwargs.get('ignore_hash_file', False)
+        target, has_hash_file = cls._has_hash_file(target, **kwargs)
+
+        if ignore_hash_file:
+            logger.info('hash file is being ignored, directory will be rehashed.')
+
+        if has_hash_file and not ignore_hash_file:
+            # FIXME: this needs to be prettied up.
+            dir_source = source
+            json_target = target
+            # FIXME: THIS DOES NOT CLEAN UP AFTER ITSELF - dir_source is not deleted after zipping etc.
+            return cls._JSON_SOURCE_DIRECTORY_TARGET_CLS(source_json=json_target, target_dir=dir_source, **kwargs)
+        return None
+
+    @classmethod
+    def _directory_src_targets(cls, source: Any, target: Any, **kwargs):
+        print("*************************************************************************************************\n"
+              "IGNORE HASH FILE IS SET TO TRUE BY DEFAULT, THIS MEANS THE DIRECTORY WILL BE REHASHED EVERY TIME.\n"
+              "*************************************************************************************************\n")
+        kwargs.setdefault('ignore_hash_file', True)
         target_is_directory = cls._is_directory_input(target)
 
         target, was_unzipped = cls._detect_and_unzip_archive(target, **kwargs)
@@ -103,14 +123,9 @@ class AutoBackupComparerFactory(ComparerFactory):
             target_is_directory = cls._is_directory_input(target)
 
         if target_is_directory:
-            target, has_hash_file = cls._has_hash_file(target, **kwargs)
-            if has_hash_file and not ignore_hash_file:
-                # FIXME: this needs to be prettied up.
-                print("has_hash_file")
-                dir_source = source
-                json_target = target
-                # FIXME: THIS DOES NOT CLEAN UP AFTER ITSELF - dir_source is not deleted after zipping etc.
-                return cls._JSON_SOURCE_DIRECTORY_TARGET_CLS(source_json=json_target, target_dir=dir_source, **kwargs)
+            hash_file_class = cls._use_hash_file(source, target, **kwargs)
+            if hash_file_class is not None:
+                return hash_file_class
 
             return cls._DIRECTORY_SOURCE_DIRECTORY_TARGET_CLS(
                 source_dir=source,
